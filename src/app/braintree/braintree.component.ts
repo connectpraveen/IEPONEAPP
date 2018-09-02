694e2c65c4546bf45ba9e21e85e59fc70047f4cc
connectpraveen <connectpraveen@gmail.com>
2018-09-02 18:49:47 +0530

    commit

--- src/app/braintree/braintree.component.ts
+++ src/app/braintree/braintree.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../providers/account.service';
+import { Account } from '../providers/account';
+import { LoginService } from '../providers/login/login.service';
+import { Login } from '../providers/login/login';
+import { SharedDataService } from '../shared/shared-data.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from '../providers/auth.service';
+import { Subscription, Profile } from '../providers/subscription/subscription';
+import { ProfileService } from '../providers/profile/profile.service';
+import { DatePipe } from '@angular/common';
+import { SubscribeService } from '../providers/subscription/subscribe.service';
+import { UserAssociateEmail, UserAssociatePhone } from '../account/user-associate-email';
+import { AccountEmailFirebaseService } from '../providers/firebase/account-email-firebase.service';
+import { AccountGmailFirebaseService } from '../providers/firebase/account-gmail-firebase.service';
 
 @Component({
   selector: 'app-braintree',
   templateUrl: './braintree.component.html',
-  styleUrls: ['./braintree.component.scss']
+  styleUrls: ['./braintree.component.scss'],
+  providers: [AccountService, SharedDataService, LoginService, AuthService, SubscribeService, ProfileService, DatePipe]
 })
 export class BraintreeComponent implements OnInit {
-
+  private currentUser: firebase.User;
   paymentResponse:any;
-  constructor() { }
+  authState: any = null;
+  name:string;
+    constructor(private accser: AccountService, private shared: SharedDataService,
+    private logSer: LoginService, public afAuth: AngularFireAuth,
+    private authService: AuthService, private profSer: ProfileService, private sharedService: SharedDataService, private subSer: SubscribeService,
+    private datePipe: DatePipe, private accSer: AccountService, private accountEmailFirebaseService: AccountEmailFirebaseService,
+    private accountGmailFirebaseService: AccountGmailFirebaseService) {
+    this.afAuth.authState.subscribe((auth) => {
+      this.authState = auth
+      this.currentUser = this.afAuth.auth.currentUser
+    });
+  }
 
   ngOnInit() {
+    this.name= this.afAuth.auth.currentUser.displayName;
+    
   }
   onPaymentStatus(response):void
   {
